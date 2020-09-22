3620fa878020a100b8a7976e8cb175b2df776e01
Viacheslav Lykhohub <s.likhogub@gmail.com>
2020-09-22 20:41:38 +0300

    Implemented CAN

---
+++ src/abstractCAN.c
@@ -0,0 +1,93 @@
+#include "abstractCAN.h"
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/can.h>
+
+#include <stddef.h>
+
+static uint32_t abst_opencm_can_conv(uint8_t can)
+{
+    switch (can) {
+        case 1:
+            return CAN1;
+            break;
+        case 2:
+            return CAN2;
+            break;
+        default:
+            return NULL;
+    }
+}
+
+/**
+ * Initialize the CAN
+ * 
+ * :param: Pointer to a :c:type:`abst_can` with filled parameters
+ * :return: Error code according to :c:type:`abst_errors`.
+ */ 
+enum abst_errors abst_can_init(const struct abst_can *can)
+{
+    uint32_t opencm_can = abst_opencm_can_conv(can->can_num);
+    if (!opencm_can)
+        return ABST_WRONG_PARAMS;
+    
+    rcc_periph_clock_enable(RCC_CAN);
+    
+    can_reset(opencm_can);
+    
+    bool init_status = can_init(opencm_can,
+                                can->ttcm, // Time triggered comm mode
+                                can->abom,  // Automatic bus-off management
+                                can->awum, // Automatic wakeup mode
+                                can->nart, // No automatic retransmission
+                                can->rflm, // Receive FIFO locked mode
+                                can->txfp, // Transmit FIFO priority
+                                can->sjw, //  Resynchronization jump width
+                                can->ts1, // Time segment 1
+                                can->ts2, // Time segment 2
+                                can->brp, // Baud rate prescaler
+                                can->loopback, // Loop back mode
+                                can->silent // Silent mode
+                               );
+    if (init_status)
+        return ABST_OPERATION_FAILED;
+    
+    return ABST_OK;
+}
+
+/**
+ * Initialize CAN 32 bit filter
+ * 
+ * :param: Pointer to a :c:type:`abst_can_filter_32_bit` with filled parameters
+ * :return: Error code according to :c:type:`abst_errors`.
+ */
+void abst_can_init_filter_32_bit(const struct abst_can_filter_32_bit *filter)
+{
+    can_filter_id_list_32bit_init(filter->filter_id,
+                                  filter->id1,
+                                  filter->id2,
+                                  filter->fifo,
+                                  filter->enable);
+}
+
+/**
+ * Get number of messages are pending in the receive FIFO.
+ * 
+ * :param can_num: Number of CAN (1, 2).
+ * :param fifo_num: Number of FIFO (1, 2).
+ * :return: Number of messages are pending in the receive FIFO.
+ */
+uint8_t abst_can_get_fifo_pending(uint8_t can_num, uint8_t fifo_num)
+{
+    uint32_t opencm_can = abst_opencm_can_conv(can_num);
+    if (fifo_num == 0)
+        return CAN_RF0R(opencm_can) & CAN_RF0R_FMP0_MASK;
+    else if (fifo_num == 1)
+        return CAN_RF1R(opencm_can) & CAN_RF1R_FMP1_MASK;
+    else
+        return 0;
+}
+
+// void abst_can_read_fifo(uint8_t fifo, uint8_t data[], uint8_t N)
+// {
+//     
+// }
